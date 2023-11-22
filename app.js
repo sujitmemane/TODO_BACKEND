1887f08ef1ea2c87284fb04d9c44f9a3c264ddc8
Sujit Memane <114643903+sujitmemane@users.noreply.github.com>
2023-11-23 00:45:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ export  const  app = express()
 
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
