8a5725e198abe2bb58bd48343fbd3f5158e48c9a
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-11-23 11:31:57 +0530

    Add

--- app.js
+++ app.js
@@ -8,28 +8,21 @@ import cors from "cors"
 
 
 dotenv.config({
-    path:"./data/config.env"
+    path: "./data/config.env"
 })
 
 
 
 
-export  const  app = express()
-
-
-app.use(
-  cors({
+export const app = express()
+app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
 
-
+}))
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/v1/users",userRouter)
-app.use("/api/v1/tasks",taskRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tasks", taskRouter)
 
 
 app.get("/me", (req, res) => {
