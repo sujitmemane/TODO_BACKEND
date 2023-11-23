ca1a6462a3a3b88dbcb49cf815f982decf24fdaf
Sujit Memane <114643903+sujitmemane@users.noreply.github.com>
2023-11-23 10:48:58 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -9,8 +9,8 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
       .cookie("token", token,{
         httpOnly: true,
         maxAge: 15 * 60 * 1000,
-        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
-        secure: process.env.NODE_ENV === "Development" ? false : true,
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "development" ? false : true,
       })
       .json({
         success: true,
